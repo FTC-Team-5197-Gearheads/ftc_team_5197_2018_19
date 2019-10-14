1aebe2dfc4b4e0842e8dcf4cae41ea3fa7037069
Lorenzo Pedroza <lorenzo.apedroza2020@gmail.com>
2019-10-14 10:31:26 -0700

    Moved universal code into a library

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/gearheads/roverruckus/FTCRobotLocationService.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.gearheads.roverruckus;
+
+import android.graphics.drawable.shapes.Shape;
+
+public class FTCRobotLocationService {
+    FTCRobotLocationService(Shape robotShape){
+
+    }
+
+
+
+
+}
